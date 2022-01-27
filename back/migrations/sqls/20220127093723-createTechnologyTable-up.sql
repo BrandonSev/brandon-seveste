cb721f08c81686e9ef29411a52fe68c5d65da077
BrandonSev <seveste.brandon@gmail.com>
2022-01-27 11:51:19 +0100

    refactor(back): ajout ON DELETE CASCADE migrations

--- back/migrations/sqls/20220127093723-createTechnologyTable-up.sql
+++ back/migrations/sqls/20220127093723-createTechnologyTable-up.sql
@@ -3,5 +3,5 @@ CREATE TABLE technology (
     `name` VARCHAR(255) NOT NULL,
     `logo` VARCHAR(255) NOT NULL,
     `category_id` INT NOT NULL,
-    FOREIGN KEY (category_id) REFERENCES category(id)
+    FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE CASCADE
 );
--- back/migrations/sqls/20220127093921-CreateUnderCategoryTable-up.sql
+++ back/migrations/sqls/20220127093921-CreateUnderCategoryTable-up.sql
@@ -2,5 +2,5 @@ CREATE TABLE under_category (
     id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(255) NOT NULL,
     category_id INT NOT NULL,
-    FOREIGN KEY (category_id) REFERENCES category(id)
+    FOREIGN KEY (category_id) REFERENCES category(id) ON DELETE CASCADE
 );
